319d0228e82bd15830f4e27777915cd1ed210832
zeromake <a390720046@gmail.com>
2022-12-18 20:02:17 +0800

    fix: del android demo

--- project/android-demo/app/src/main/java/com/zeromake/onscripter/MainActivity.kt
+++
@@ -1,9 +0,0 @@
-package com.zeromake.onscripter
-
-import org.libsdl.app.SDLActivity
-
-class MainActivity : SDLActivity() {
-    override fun getLibraries(): Array<String> {
-        return arrayOf("main")
-    }
-}
