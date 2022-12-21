ea57541c273d40ca3cf54f27f2d685a879bc2401
zeromake <a390720046@gmail.com>
2022-12-21 19:08:12 +0800

    fix: searchSaveFile use std::filesystem::exists

--- project/ios/onscripter-oc/onscripter/ViewController.h
+++ project/ios/onscripter-oc/onscripter/ViewController.h
@@ -7,8 +7,10 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface ViewController : UIViewController<UITableViewDataSource,UITableViewDelegate> {
+    IBOutlet UITableView *tableView;
+    NSMutableArray *data;
+}
 
 @end
 
