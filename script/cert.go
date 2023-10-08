9800d84f288bfcccc66df9c54ffd4ec2960fb62d
zeromake <a390720046@gmail.com>
2023-10-08 14:17:04 +0800

    fix: cert

--- script/cert.go
+++ script/cert.go
@@ -8,13 +8,13 @@ import (
 
 func main() {
 	var cert = os.Args[1]
-	var out = os.Args[2]
+	var out = "./cert.tar.xz"
 	log.Println("cert: ", cert[0:4])
 	bb, err := base64.StdEncoding.DecodeString(cert)
 	if err != nil {
 		log.Fatal(err)
 	}
-	f, err := os.OpenFile(out, os.O_CREATE|os.O_WRONLY, 0644)
+	f, err := os.OpenFile(out, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
 	if err != nil {
 		log.Fatal(err)
 	}
