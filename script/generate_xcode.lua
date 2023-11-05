fba9088a553ac23e7e4b202ee4d2d6580ac5f61e
zeromake <a390720046@gmail.com>
2023-11-05 22:23:08 +0800

    fix: update generate xproj

--- script/generate_xcode.lua
+++ script/generate_xcode.lua
@@ -1,4 +1,10 @@
 import("duplicate")
+import("core.base.bytes")
+
+
+local function to_hash(name)
+    return string.upper(string.sub(hash.md5(bytes(name)), 1, 24))
+end
 
 function main(target)
     local links = duplicate.new()
@@ -55,4 +61,115 @@ function main(target)
     print("link_dirs:", link_dirs.value)
     print("include_dirs:", include_dirs.value)
     print("frameworks:", frameworks.value)
+    local ref_hash = {}
+    -- local
+    local app_hash = to_hash("onscripter.app")
+    ref_hash["onscripter.app"] = app_hash
+
+    local SOURCE_REFS = {}
+    table.insert(
+        SOURCE_REFS,
+        app_hash.." /* onscripter.app */ = {isa = PBXFileReference; explicitFileType = wrapper.application; includeInIndex = 0; path = onscripter.app; sourceTree = BUILT_PRODUCTS_DIR; };"
+    )
+    for _, f in ipairs({
+        "ViewController.h",
+        "ViewController.m",
+        "Assets.xcassets",
+        "Info.plist",
+        "main.mm",
+        "Main.storyboard",
+    }) do
+        local lastKnownFileType = "sourcecode.c.h"
+        if f:endswith(".m") then
+            lastKnownFileType = "sourcecode.c.objc"
+        elseif f:endswith(".mm") then
+            lastKnownFileType = "sourcecode.c.objcpp"
+        elseif f:endswith(".plist") then
+            lastKnownFileType = "text.plist.xml"
+        elseif f:endswith(".xcassets") then
+            lastKnownFileType = "folder.assetcatalog"
+        elseif f:endswith(".storyboard") then
+            lastKnownFileType = "file.storyboard"
+        end
+        local current_hash = to_hash(bytes(f))
+        ref_hash[f] = current_hash
+        table.insert(
+            SOURCE_REFS,
+            string.format(
+                '%s /* %s */ = {isa = PBXFileReference; lastKnownFileType = %s; path = %s; sourceTree = "<group>"; };\n',
+                current_hash,
+                f,
+                lastKnownFileType,
+                f
+            )
+        )
+    end
+
+    for _, f in ipairs(frameworks.value) do
+        local k = f..".framework"
+        local current_hash = to_hash(bytes(k))
+        ref_hash[k] = current_hash
+
+        table.insert(
+            SOURCE_REFS,
+            (string.format(
+                '%s /* %s */ = {isa = PBXFileReference; lastKnownFileType = wrapper.framework; name = %s; path = System/Library/Frameworks/%s; sourceTree = SDKROOT; };\n',
+                current_hash,
+                k,
+                k,
+                k
+            ))
+        )
+    end
+
+    local BUILD_SECTION_FILES = {}
+    local build_section_template = '%s /* %s */ = {isa = PBXBuildFile; fileRef = %s; };\n'
+
+    for _, name in ipairs({
+        "ViewController.m",
+        "main.mm",
+        "Main.storyboard"
+    }) do
+        local k = name..".build_section"
+        local current_hash = to_hash(k)
+        table.insert(BUILD_SECTION_FILES, string.format(
+            build_section_template,
+            current_hash,
+            name.." in Sources",
+            ref_hash[name]
+        ))
+        ref_hash[k] = current_hash
+    end
+
+    for _, name in ipairs(frameworks.value) do
+        name = name..".framework"
+        local k = name..".build_section"
+        local current_hash = to_hash(k)
+        table.insert(BUILD_SECTION_FILES, string.format(
+            build_section_template,
+            current_hash,
+            name.." in Frameworks",
+            ref_hash[name]
+        ))
+        ref_hash[k] = current_hash
+    end
+
+    local FRAMEWORKS_BUILD_PHASE = {}
+    local framework_build_phase_template = '%s /* %s in Frameworks */,\n'
+    for _, name in ipairs(frameworks.value) do
+        name = name..".framework"
+        local k = name..".build_section"
+        table.insert(FRAMEWORKS_BUILD_PHASE, string.format(
+            framework_build_phase_template,
+            ref_hash[k],
+            name
+        ))
+    end
+
+    local PBXGROUP_SECTION = {}
+
+    print("SOURCE_REFS:\n", SOURCE_REFS)
+    print("BUILD_SECTION_FILES:\n", BUILD_SECTION_FILES)
+    print("FRAMEWORKS_BUILD_PHASE:\n", FRAMEWORKS_BUILD_PHASE)
+    print(ref_hash)
 end
