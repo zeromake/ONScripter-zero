a2a836493f9d51df7d2b8fa2fcc959f7d5615214
zeromake <a390720046@gmail.com>
2023-11-11 22:57:46 +0800

    feat: add GraphicsMagick resize done

--- demo/resize.cpp
+++ demo/resize.cpp
@@ -1,4 +1,5 @@
-#include "resize.h"
+#include "resize/SDL_resize.h"
+
 
 #include <SDL.h>
 #include <SDL_image.h>
@@ -48,9 +49,10 @@ int main(int argc, char* argv[]) {
                                             fmt->Amask);
     printf("src_img: %d x %d\n", src_img->w, src_img->h);
     printf("img: %d x %d\n", img->w, img->h);
-    ResizeImage(src_img, img, UndefinedFilter, 1.0);
+    SDLSurfaceResize(src_img, img, UndefinedFilter, 1.0);
     // SDL_UpperBlitScaled(src_img, NULL, img, NULL);
-    SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, img);
+    SDL_Texture* texture1 = SDL_CreateTextureFromSurface(renderer, src_img);
+    SDL_Texture* texture2 = SDL_CreateTextureFromSurface(renderer, img);
     while (!exit) {
         while (SDL_PollEvent(&event)) switch (event.type) {
                 case SDL_QUIT:
@@ -58,18 +60,20 @@ int main(int argc, char* argv[]) {
                     break;
             }
 
-        SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0xff);
+        SDL_SetRenderDrawColor(renderer, 0xff, 0xff, 0xff, 0xff);
         SDL_RenderClear(renderer);
 
-        SDL_Rect imgRect = {0, 0, img->w, img->h};
+        SDL_Rect imgRectSrc1 = {0, 0, src_img->w, src_img->h};
+        SDL_Rect imgRect1 = {0, 0, src_img->w, src_img->h};
+        SDL_Rect imgRectSrc2 = {0, 0, img->w, img->h};
+        SDL_Rect imgRect2 = {img->w, 0, img->w, img->h};
 
-        SDL_UpdateTexture(
-            texture, &imgRect, (unsigned char*)img->pixels, img->pitch);
-
-        SDL_RenderCopy(renderer, texture, &imgRect, &imgRect);
+        SDL_RenderCopy(renderer, texture1, &imgRectSrc1, &imgRect1);
+        SDL_RenderCopy(renderer, texture2, &imgRectSrc2, &imgRect2);
         SDL_RenderPresent(renderer);
     }
-    SDL_DestroyTexture(texture);
+    SDL_DestroyTexture(texture1);
+    SDL_DestroyTexture(texture2);
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
---
+++ src/resize/SDL_resize.h
@@ -0,0 +1,30 @@
+#ifndef _SDL_RESIZE_H
+#define _SDL_RESIZE_H
+
+#if defined(__cplusplus) || defined(c_plusplus)
+extern "C" {
+#endif
+
+#include <SDL.h>
+#include "resize.h"
+
+int SDLSurfaceResize(
+    SDL_Surface *src,
+    SDL_Surface *dst,
+    const FilterTypes filter,
+    const double blur
+);
+
+SDL_Surface* SDLSurfaceResizeWrap(
+    SDL_Surface *src,
+    Uint64 w,
+    Uint64 h,
+    const FilterTypes filter,
+    const double blur
+);
+
+#if defined(__cplusplus) || defined(c_plusplus)
+}
+#endif
+
+#endif
